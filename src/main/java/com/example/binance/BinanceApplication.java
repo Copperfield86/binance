5f195a866c5bb38a29b386d3bcfe944501cc094c
Dave <mannfred_von-carstein@protonmail.com>
2025-01-27 10:28:20 +0100

    Tak

--- src/main/java/com/example/binance/BinanceApplication.java
+++ src/main/java/com/example/binance/BinanceApplication.java
@@ -1,3 +1,4 @@
+
 package com.example.binance;
 
 import org.springframework.boot.SpringApplication;
--- src/main/java/com/example/binance/repository/AlertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.binance.repository;
-
-import com.example.binance.model.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlertRepository extends JpaRepository<Alert, Long> {
-}
